12cf100a21a61dfb1fccec4e561eb1f421152b79
rdwanmuhamad <mrdwn624@gmail.com>
2026-04-01 16:05:59 +0800

    slicing template backend

---
+++ resources/views/includes/admin/script.blade.php
@@ -0,0 +1,15 @@
+ <script src="{{ asset('build/assets/backend/vendor/jquery/jquery.min.js') }}"></script>
+ <script src="{{ asset('build/assets/backend/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+
+ <!-- Core plugin JavaScript-->
+ <script src="{{ asset('build/assets/backend/vendor/jquery-easing/jquery.easing.min.js') }}"></script>
+
+ <!-- Custom scripts for all pages-->
+ <script src="{{ asset('build/assets/backend/js/sb-admin-2.min.js') }}"></script>
+
+ <!-- Page level plugins -->
+ <script src="{{ asset('build/assets/backend/vendor/chart.js/Chart.min.js') }}"></script>
+
+ <!-- Page level custom scripts -->
+ <script src="{{ asset('build/assets/backend/js/demo/chart-area-demo.js') }}"></script>
+ <script src="{{ asset('build/assets/backend/js/demo/chart-pie-demo.js') }}"></script>
--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,13 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('home');
+    return view('welcome');
 });
 
-Route::get('/about', function () {
-    return view('about');
+Route::get('/admin', function () {
+    return view('pages.admin.dashboard');
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
